43bf45461748fc7d842217a7786a853ed0bdeb01
Ben Lieberman <bhlieberman93@gmail.com>
2023-01-14 09:23:01 -0700

    query data moved to edn file

--- src/main/com/slothrop/statcast/batter.clj
+++ src/main/com/slothrop/statcast/batter.clj
@@ -2,24 +2,14 @@
   (:require
    [clojure.java.io :as io] 
    [clojure.string :as string]
+   [clojure.edn :refer [read-string]]
    [charred.api :refer [read-csv]]
    [ring.util.response :refer [response]]
    [com.slothrop.http.client :refer [split-query send-split-reqs]]))
 
 (def query-defaults
-  {:sort-col "pitches" :hfSit nil :hfPT nil,
-   :hfOutfield nil :game-date-gt nil :min-pitches "0",
-   :metric-1 nil :hfSA nil :hfInn nil,
-   :hfTeam nil :game-date-lt nil :batter-stands nil,
-   :sort-order "desc" :hfOuts nil :hfStadium nil,
-   :player-event-sort "api_p_release_speed" :hfZ nil :type "details",
-   :player-type "batter" :hfGT "R%7C" :hfC nil,
-   :home-road nil :min-results "0" :hfRO nil,
-   :hfOpponent nil :pitcher-throws nil :hfPull nil,
-   :hfFlag nil :min-pas "0" :group-by "name",
-   :hfBBT nil :position nil :hfSea "2022%7C",
-   :hfInfield nil :hfMo nil :hfPR nil,
-   :hfBBL nil :hfAB nil :hfNewZones nil})
+  (with-open [rdr (-> "public/query.edn" io/resource io/reader)]
+    (read-string (slurp rdr))))
 
 (defn make-query-map [defaults {:keys [date-start? date-end? team?] :as params}]
   (cond-> defaults
