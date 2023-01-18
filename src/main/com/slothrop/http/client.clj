2d32e6226565fc9de9a242827130a118c594f880
Ben Lieberman <bhlieberman93@gmail.com>
2023-01-17 19:02:38 -0700

    moved dates function to new ns

--- src/main/com/slothrop/http/client.clj
+++ src/main/com/slothrop/http/client.clj
@@ -1,12 +1,10 @@
 (ns com.slothrop.http.client 
   (:require [clojure.core.async :refer [go chan alts!! >!]]
-            [clojure.spec.alpha :as s]
+            #_[clojure.spec.alpha :as s]
             [clj-http.client :as client] 
-            [ring.util.response :refer [response]])
-  (:import [java.time LocalDate]
-           [java.time.format DateTimeFormatter]))
+            [ring.util.response :refer [response]]))
 
-(defn send-split-reqs [reqs]
+(defn send-split-reqs ^{:deprecated true} [reqs]
   (let [c (count reqs) cs (repeatedly c chan)]
     (doseq [req reqs]
       (go (>! (rand-nth cs)
@@ -16,16 +14,3 @@
         (let [[v _] (alts!! cs)]
           (recur (inc i) (conj result v)))
         result))))
-
-(defn split-dates [date-start date-end]
-  {:pre [(s/valid? string? date-start) (s/valid? string? date-end)]
-   :post [(s/valid? (s/coll-of (s/map-of #{:game-date-gt :game-date-lt} 
-                                #(instance? LocalDate %))) %)]}
-  (let [format (DateTimeFormatter/ofPattern "yyyy-MM-dd")
-        ds (-> (LocalDate/parse date-start format)
-               (.datesUntil (LocalDate/parse date-end format))
-               .iterator
-               iterator-seq)
-        split-ds (if (even? (count ds)) (partition 2 ds) (partition 3 ds))]
-    (map (fn [d] (assoc {} :game-date-gt (first d)
-                        :game-date-lt (second d))) split-ds)))
