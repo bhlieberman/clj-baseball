2aed46beb9a2f0f12386118190a6e727a717e980
Ben Lieberman <bhlieberman93@gmail.com>
2023-01-24 11:45:14 -0700

    fun with multimethods

--- src/main/com/slothrop/sabermetrics/formulas.clj
+++ src/main/com/slothrop/sabermetrics/formulas.clj
@@ -11,46 +11,50 @@
 (s/def ::pa ::pspec/plate-appearances) ; plate appearances
 (s/def ::sac int?) ; sacrifices hits or bunts
 (s/def ::interference int?) ; batter reached on interference
-(s/def ::at-bats-args (s/keys :req-un [::bb ::hbp ::pa
-                                       ::sac ::interference]))
-(defn at-bats [m]
-  (let [conf (s/conform ::at-bats-args m)]
-    (if (s/invalid? conf)
-      (throw (ex-info "Missing a value needed to compute at bats."
-                      (->> m 
-                           (s/explain-data ::at-bats-args) 
-                           :clojure.spec.alpha/problems
-                           first))) 
-      (let [{:keys [pa hbp bb sac interference]} conf]
-        (- pa hbp bb sac interference)))))
+(s/def ::k int?)
 
-(s/def ::walk-rate-args (s/keys :req-un [::bb ::pa]))
-(defn walk-rate [{:keys [bb pa]}] 
-  (try (/ bb pa)
-       (catch ArithmeticException _
-         (println "plate appearances must be non-zero."))
-       (catch NullPointerException _
-         (println "one of the required values was null"))))
+(defn batting-avg [{::keys [h ab]}] (/ h ab))
+(defn k-rate [{::keys [k pa]}] (float (/ k pa)))
+(defn bb-rate [{::keys [bb pa]}] (float (/ bb pa)))
 
-(s/def ::k int?) ; strikeouts
-(s/def ::strikeout-rate-args (s/keys :req-un [::k ::pa]))
-(defn strikeout-rate [{:keys [k pa]}]
-  (try (/ k pa)
-       (catch ArithmeticException _
-         (println "plate appearances must be non-zero."))
-       (catch NullPointerException _
-         (println "one of the required values was null"))))
-
-(defn walk-strikeout-ratio [{:keys [bb k]}] (/ bb k))
-
-(defn batting-avg [{:keys [h ab]}] (/ h ab))
-
-(defn obp [{:keys [h bb hbp ab sf]}]
+(defn obp [{::keys [h bb hbp ab sf]}]
   (/ (+ h bb hbp) (+ ab bb hbp sf)))
 
-(defn slugging [{:keys [singles doubles triples home-runs ab]}]
+(defn slugging [{::keys [singles doubles triples home-runs ab]}]
   (/ (+ singles doubles triples home-runs) ab))
 
+(s/def :outcome/type keyword?)
+(defmulti at-bat :outcome/type)
+(defmethod at-bat :outcome/walk [_]
+  (s/keys :req [::bb ::pa ::k]))
+(defmethod at-bat :outcome/k [_]
+  (s/keys :req [::k ::bb ::pa]))
+(defmethod at-bat :outcome/single [_]
+  (s/keys :req [:pspec/avg :pspec/obp :pspec/slg]))
+(defmethod at-bat :outcome/double [_]
+  (s/keys :req [:pspec/avg :pspec/obp :pspec/slg]))
+(defmethod at-bat :outcome/triple [_]
+  (s/keys :req [:pspec/avg :pspec/obp :pspec/slg]))
 
-(at-bats {:bb 1 :hbp 2 :sac 3 :interference 4})
-(walk-rate {:bb 1})
+
+(s/def :outcome/outcome (s/multi-spec at-bat :outcome))
+
+(defmulti at-bat-outcome (fn [m] (:outcome/type m)))
+(defmethod at-bat-outcome :outcome/walk [v] {:walks (inc (::bb v)) 
+                                             :walk-rate (bb-rate v)})
+(defmethod at-bat-outcome :outcome/k [v] {:k (inc (::k v)) :k-rate (k-rate v)})
+(defmethod at-bat-outcome :outcome/single [v]
+  (let [singles (inc (::singles v)) avg (/ (::hits v) (::ab v))]
+    {:singles singles :batting-average avg}))
+
+(defmulti dispatch-f class)
+(defmethod dispatch-f clojure.lang.PersistentArrayMap [v]
+  (at-bat-outcome v))
+(defmethod dispatch-f clojure.lang.Keyword [_]
+  (throw (ex-info "Did not pass spec :outcome/outcome" {})))
+
+(dispatch-f (s/conform :outcome/outcome {:outcome/type :outcome/k
+                                         ::bb 1
+                                         ::pa 14
+                                         ::k 4}))
+
