4e2e9d015b85b4fb059145e63b8b3678206ea24d
Ben Lieberman <bhlieberman93@gmail.com>
2023-01-29 10:03:03 -0700

    new df creation fn tested against big dataset

--- resources/user.clj
+++ resources/user.clj
@@ -1,9 +1,14 @@
-(ns resources.user
-  (:require [portal.api :as p]
-            [clojure.repl :refer [source doc]]
-            [clojure.java.javadoc :refer [javadoc]]))
+(ns resources.user 
+  (:require [portal.api :as p] 
+            [com.slothrop.interop.py-config :refer [create-dataframe]]
+            [com.slothrop.statcast.batter :refer [send-req]])
+  (:import [java.net URLEncoder]))
 
 (def p (p/open {:launcher :vs-code}))
 (add-tap #'p/submit)
 
-(javadoc System)
+(def results (send-req {:game-date-gt "2022-05-01"
+                        :game-date-lt "2022-05-02"
+                        :hfTeam (URLEncoder/encode "BAL|" "utf-8")}))
+
+(create-dataframe results)
