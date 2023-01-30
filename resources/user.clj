89ced898058c50bfcea41f725a26571f9f4ca4cb
Ben Lieberman <bhlieberman93@gmail.com>
2023-01-30 08:10:01 -0700

    updated send-req fn and corresponding scratch code

--- resources/user.clj
+++ resources/user.clj
@@ -1,14 +1,20 @@
 (ns resources.user 
-  (:require [portal.api :as p] 
-            [com.slothrop.interop.py-config :refer [create-dataframe]]
-            [com.slothrop.statcast.batter :refer [send-req]])
+  (:require [portal.api :as p]
+            [clojure.spec.alpha :as s]
+            [com.slothrop.interop.dataframes :refer [create-dataframe-from-dict
+                                                     create-dataframe-from-records]]
+            [com.slothrop.statcast.batter :refer [send-req!]]
+            [com.slothrop.statcast.results-spec :as rspec])
   (:import [java.net URLEncoder]))
 
 (def p (p/open {:launcher :vs-code}))
 (add-tap #'p/submit)
 
-(def results (send-req {:game-date-gt "2022-05-01"
+(def results (send-req! {:game-date-gt "2022-05-01"
                         :game-date-lt "2022-05-02"
                         :hfTeam (URLEncoder/encode "BAL|" "utf-8")}))
 
-(create-dataframe results)
+(def test-map (first results))
+
+(create-dataframe-from-records (take 20 results))
+
--- src/com/slothrop/statcast/batter.clj
+++ src/com/slothrop/statcast/batter.clj
@@ -14,18 +14,18 @@
     (read-string (slurp rdr))))
 
 (defn make-query-map
-  "Modifies the query map stored in query-defaults with a user-specified
-   map of search parameters."
+  {:doc "Modifies the query map stored in query-defaults with a user-specified
+   map of search parameters."}
   [defaults params]
   #_{:pre [(s/valid? :com.slothrop.statcast.specs/query params)]
    :post [(s/valid? :com.slothrop.statcast.specs/query %)]}
   (merge defaults params))
 
 (defn make-query-string
-  "Turns the map of query parameters into a query string compliant with
+  {:doc "Turns the map of query parameters into a query string compliant with
    Statcast's query endpoint. Does NOT perform URL encoding except
    where necessary. Arbitrary data that does not conform to the range of accepted
-   values for a parameter will not modify the results."
+   values for a parameter will not modify the results."}
   [kvs]
   (letfn [(underscores [k] (-> k name (string/replace #"-" "_")))
           (make-qs [acc k v] (cond-> acc
@@ -33,8 +33,23 @@
                                (some? v) (str (underscores k) "=" v "&")))]
     (-> make-qs (reduce-kv "" kvs) (str "all=true"))))
 
-(defn send-req [params]
+(defn- parse-double-vals [m]
+  (let [ks (select-keys m [:ax :ay :az :vx0 :vy0 :vz0])]
+    (merge m (zipmap (keys ks) (map parse-double (vals ks))))))
+
+(defn- parse-int-vals [m]
+  (let [ks (select-keys m [:batter :zone :inning :balls :pitcher :strikes])]
+    (merge m (zipmap (keys ks) (map parse-long (vals ks))))))
+
+(defn send-req! 
+  {:doc "Sends the composed and spec'ed query to Statcast."}
+  [params]
+  {:post [(every? #(s/valid? :com.slothrop.statcast.results-spec/results %) %)]}
   (let [url "https://baseballsavant.mlb.com/statcast_search/csv?" 
         qs (->> params (make-query-map query-defaults) make-query-string (str url))
-        results (-> qs client/get response :body :body read-csv)]
-    (map (partial zipmap (first results)) (rest results))))
+        results (-> qs client/get response :body :body read-csv)
+        cols (map keyword (first results))]
+    (->> (rest results) 
+         (map (comp parse-int-vals 
+                    parse-double-vals 
+                    (partial zipmap cols))))))
