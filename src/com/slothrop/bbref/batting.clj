48c29012e5d25974860c056ab25f8108717774e0
Ben Lieberman <bhlieberman93@gmail.com>
2023-02-03 16:33:58 -0700

    added docs and more type hints to avoid reflection

--- src/com/slothrop/bbref/batting.clj
+++ src/com/slothrop/bbref/batting.clj
@@ -3,24 +3,37 @@
 
 (set! *warn-on-reflection* true)
 
-(defn bbref-conn [url] (Jsoup/connect url))
+(defn bbref-conn
+  {:doc "Opens a JSoup Connection to the URL."}
+  ^org.jsoup.Connection [url] (Jsoup/connect url))
 
-(defn doc [url] (.get (bbref-conn url)))
+(defn doc
+  {:doc "Executes a GET request on the open Connection and returns the HTML data as a JSoup Document."}
+  [url] (.get (bbref-conn url)))
 
-(defn get-batting-table [^org.jsoup.nodes.Document doc] 
-  (some-> doc 
-      (.getElementById "batting_standard")
-      .children))
+(defn get-batting-table
+  {:doc "Returns the Standard Batting table from a player's BBRef profile."}
+  [^org.jsoup.nodes.Document doc]
+  (.. doc (getElementById "batting_standard") children))
 
-(defn html->str [doc] (->> doc .text (re-seq #"\w+") (partition 32)))
+(defn html->str
+  {:doc "Creates a string representation of tabular data provided as a Document."}
+  [^org.jsoup.nodes.Document doc]
+  (->> doc .text (re-seq #"\w+") (partition 32)))
 
 (defn data [doc] (-> doc get-batting-table html->str))
 
-(def str->cols (comp
-                (take 1)
-                (mapcat identity)
-                (filter (complement #{"Standard" "Batting"}))))
+(def str->cols
+  "A transducer to filter column names from the raw HTML text string."
+  (comp
+   (take 1)
+   (mapcat identity)
+   (filter (complement #{"Standard" "Batting"}))))
 
-(defn cols [doc] (->> doc data (transduce str->cols conj)))
+(defn cols
+  {:doc "Runs the str->cols transducer to extract column names."}
+  [doc] (->> doc data (transduce str->cols conj)))
 
-(defn rows [data] (into [] (eduction (map vec) (drop 1) data)))
+(defn rows 
+  {:doc "Returns a vector of the table rows."}
+  [data] (into [] (eduction (map vec) (drop 1) data)))
