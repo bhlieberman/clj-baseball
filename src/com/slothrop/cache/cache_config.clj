4e22b233d301b47ade2cfc3a57dd65d520428357
Ben Lieberman <bhlieberman93@gmail.com>
2023-08-17 16:53:34 -0600

    setting up caching

---
+++ src/com/slothrop/cache/cache_config.clj
@@ -0,0 +1,15 @@
+(ns com.slothrop.cache.cache-config
+  (:import [java.nio.file Paths]))
+
+(def ^:dynamic *cache-enabled?* false)
+
+(defn enable []
+  (set! *cache-enabled?* true))
+
+(defn disable []
+  (set! *cache-enabled?* false))
+
+(def DEFAULT-CACHE-DIR 
+  (Paths/get (System/getProperty "user.home") 
+             (into-array [".pybaseball" "cache"])))
+
