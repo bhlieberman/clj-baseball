38a8577bece42f9c6350f138aa2edfafe708f129
Ben Lieberman <bhlieberman93@gmail.com>
2023-08-18 11:12:28 -0600

    experimenting with FuzzySearch

--- resources/dev_user.clj
+++ resources/dev_user.clj
@@ -7,28 +7,37 @@
             [clojure.java.javadoc :refer [javadoc]]
             [clojure.java.io :as jio]
             [clojure.string :as string]
-            [clj-http.client :as client]
+            [clj-http.client :as client] 
             [tech.v3.dataset :as d]
             [tech.v3.dataset.zip :as z]
             [tech.v3.dataset.io.csv :as csv]
             [com.slothrop.statcast.batter :refer [send-req!]]
             [com.slothrop.statcast.results-spec :as-alias rspec]
             [com.slothrop.bbref.batting :refer [data rows]]
-            [com.slothrop.player.lookup :refer [lookup-player-by-mlbid table-csv]]
+            [com.slothrop.player.lookup :refer [lookup-player-by-mlbid lookup-table-v2 table-csv]]
             [com.slothrop.cache.cache-config :refer [DEFAULT-CACHE-DIR]])
-  #_{:clj-kondo/ignore [:unused-import]}
-  (:import [java.net URLEncoder URI]
-           [java.nio.file Paths]
-           [java.util.zip ZipFile ZipInputStream]))
+  (:import [java.util.zip ZipFile]
+           [me.xdrop.fuzzywuzzy FuzzySearch]))
 
-
+;; a single day of game data for dev usage
 (def results (send-req! {:game-date-gt "2022-05-01"
                          :game-date-lt "2022-05-02"
                          :hfTeam "BAL|"}))
 
+;; sample tech.ml Dataset of above query results
 (def table-data (d/->dataset results))
 
+(def lookup-table (lookup-table-v2))
 
+(defn get-closest-names [player-last player-first player-table]
+  (map #(.getString %) (FuzzySearch/extractTop (str player-first player-last) 
+                                               (into [] (:player_name player-table)) 
+                                               5)))
+
+(get-closest-names "Ripken" "cAl" lookup-table)
+
+
+lookup-table
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (def chadwick-tables
