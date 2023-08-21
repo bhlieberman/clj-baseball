a02161e2cfde8e25a2312890d5dac935e94553b4
Ben Lieberman <bhlieberman93@gmail.com>
2023-08-21 14:26:24 -0600

    spring cleaning

--- resources/dev_user.clj
+++ resources/dev_user.clj
@@ -18,9 +18,8 @@
             [com.slothrop.statcast.batter :refer [send-req!]]
             [com.slothrop.statcast.results-spec :as-alias rspec]
             [com.slothrop.bbref.batting :refer [data rows]]
-            [com.slothrop.player.lookup :refer [lookup-table-v2 table->csv]]
-            [com.slothrop.cache.cache-config :refer [DEFAULT-CACHE-DIR]])
-  (:import [java.util.zip ZipFile]))
+            [com.slothrop.player.lookup :refer [lookup-table]]
+            [com.slothrop.cache.cache-config :refer [DEFAULT-CACHE-DIR]]))
 
 ;; a single day of game data for dev usage
 (def results (send-req! {:game-date-gt "2022-05-01"
@@ -32,23 +31,6 @@
 (def table-data (d/->dataset results))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(def lookup-table (lookup-table-v2))
+(def p (p/open {:launcher :vs-code}))
 
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(def chadwick-tables
-  (let [re (-> "register-master.zip"
-               jio/resource
-               .toURI
-               jio/file
-               ZipFile.)]
-    (->> re
-         .entries
-         iterator-seq
-         (drop 3)
-         (map (fn [f] (-> re (.getInputStream f) csv/csv->dataset)))
-         (into []))))
-
-(comment #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
- (def p (p/open {:launcher :vs-code}))
-
-         (add-tap #'p/submit))
+(add-tap #'p/submit)
--- src/com/slothrop/player/lookup.clj
+++ src/com/slothrop/player/lookup.clj
@@ -10,21 +10,9 @@
             [com.slothrop.cache.cache-config :refer [DEFAULT-CACHE-DIR]])
   (:import [me.xdrop.fuzzywuzzy FuzzySearch]))
 
-(defn table->csv [table]
+(defn- table->csv [table]
   (with-open [is (jio/input-stream table)]
     (csv/csv->dataset is {:file-type :csv})))
-
-(def ^:deprecated lookup-table
-  "This will be removed in v0.2.1. Prefer Chadwick Bureau register dataset."
-  (-> (client/get "https://clj-baseball.s3.us-west-2.amazonaws.com/lookup-table"
-                  {:async? true
-                   :as :stream}
-                  (fn [response] response)
-                  (fn [error] (throw (ex-info "Request for player lookup table failed" {:cause error}))))
-      .get
-      .getEntity
-      .getContent
-      table->csv))
 
 ;; lookup table v2 from Chadwick Bureau
 (def register (client/get "https://github.com/chadwickbureau/register/archive/refs/heads/master.zip"
@@ -33,18 +21,17 @@
                           (fn [resp] resp)
                           (fn [err] (throw (ex-info "Request for player lookup table failed" {:cause err})))))
 
-(defn get-cached-register-file []
+(defn- get-cached-register-file []
   (if (.exists (jio/file (.toUri DEFAULT-CACHE-DIR)))
     (try (table->csv (str DEFAULT-CACHE-DIR "/last_query.csv"))
          (catch java.io.FileNotFoundException _))
     nil))
 
-(def register->dataset 
+(def ^:private register->dataset 
   (let [cached-ds (get-cached-register-file)]
-    (when-not cached-ds ;; account for cached file later ... if - else
-      (z/zipfile->dataset-seq (.. register get getEntity getContent)))))
+    (or cached-ds (z/zipfile->dataset-seq (.. register get getEntity getContent)))))
 
-(defn keep-cols [ds]
+(defn- keep-cols [ds]
   (try (d/select-columns ds ["name_last" "name_first" "key_mlbam"
                              "key_retro" "key_bbref" "key_fangraphs"
                              "mlb_played_first", "mlb_played_last"])
@@ -52,10 +39,10 @@
 
 (def PEOPLE_PATTERN (re-pattern ".*people\\-[0-9a-f]\\.csv"))
 
-(defn create-full-names [ds]
+(defn- create-full-names [ds]
   (d/row-map ds (fn [row] {:player-name (str (row "name_first") " " (row "name_last"))})))
 
-(defn get-closest-names [player-last player-first player-table]
+(defn- get-closest-names [player-last player-first player-table]
   (let [player-names (into [] (:player-name player-table))
         most-similar {:player-name
                       (map #(.getString %) (FuzzySearch/extractTop (str player-first player-last)
@@ -63,7 +50,7 @@
                                                                    5))}]
     (j/pd-merge (d/->dataset most-similar) player-table {:on :player-name})))
 
-(defn lookup-table-v2 []
+(def lookup-table 
   (letfn [(complete-records [ds] (m/drop-missing
                                   ds
                                   ["key_retro" "key_bbref" "key_fangraphs"
@@ -79,24 +66,18 @@
          complete-records
          create-full-names)))
 
-(defn search [{:keys [player-last player-first fuzzy?] :or {fuzzy? false}}]
-  (let [lookup (lookup-table-v2)]
-    (if fuzzy?
-      (get-closest-names player-last player-first lookup)
-      (if (nil? player-first)
-        (tcr/select-rows lookup (comp #(= % player-last) #(get % "name_last")))
-        (tcr/select-rows lookup (comp (fn [[first last]]
-                                        (and (= first player-first) (= last player-last)))
-                                      (juxt #(get % "name_first")
-                                            #(get % "name_last"))))))))
+(defn search
+  {:doc "Look up a player's MLB ID from the Chadwick Bureau dataset. Accepts a `fuzzy?` flag to enable fuzzy searching of names."}
+  [{:keys [player-last player-first fuzzy?] :or {fuzzy? false}}]
+  (if fuzzy?
+    (get-closest-names player-last player-first lookup-table)
+    (if (nil? player-first)
+      (tcr/select-rows lookup-table (comp #(= % player-last) #(get % "name_last")))
+      (tcr/select-rows lookup-table (comp (fn [[first last]]
+                                (and (= first player-first) (= last player-last)))
+                              (juxt #(get % "name_first")
+                                    #(get % "name_last")))))))
 
 (comment
   (search {:player-last "Ripken"
            :player-first "Cal"}))
-
-(comment (def keep-cols (keep (fn [[& x]] (map (partial nth x)
-                                               (conj (into [] (range 1 11)) 15))))))
-
-(defmulti ^:deprecated player-profile "DEPRECATED! USE table-csv instead! Will be removed in v.0.2.1" identity)
-(defmethod player-profile :cols [_] (into [] cat (sequence (comp (take 1) keep-cols))))
-(defmethod player-profile :rows [_] (transduce (comp (drop 1) keep-cols) conj lookup-table))
