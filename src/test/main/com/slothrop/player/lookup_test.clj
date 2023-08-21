6a219b4e33b9bc8b6cd7cc0cfac03e5febdef790
Ben Lieberman <bhlieberman93@gmail.com>
2023-08-21 14:26:08 -0600

    small testing changes to reflect var renaming

--- src/test/main/com/slothrop/player/lookup_test.clj
+++ src/test/main/com/slothrop/player/lookup_test.clj
@@ -1,16 +1,11 @@
 (ns player.lookup-test
   (:require [clojure.test :refer [deftest is testing run-tests]]
             [tech.v3.dataset :as d]
-            [com.slothrop.player.lookup :refer [lookup-table-v2
-                                                get-cached-register-file]]))
-
-(deftest cached-file-not-exists
-  (is (nil? (get-cached-register-file))))
+            [com.slothrop.player.lookup :refer [lookup-table]]))
 
 (deftest lookup-table-pipeline
-  (testing "that the full pipeline of functions correctly reduces and filters the Chadwick dataset"
-    (let [register (lookup-table-v2)]
-      (is (some? register))
-      (is (= (count (d/rows register)) 20627)))))
+  (testing "that the full pipeline of functions correctly reduces and filters the Chadwick dataset" 
+    (is (some? lookup-table))
+    (is (= (count (d/rows lookup-table)) 20627))))
 
 (run-tests)
