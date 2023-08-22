152adac0ffec8117125d1202c95e2230cfed39c2
bhlieberman <bhlieberman93@gmail.com>
2023-08-22 15:51:52 -0600

    v0.3.5

--- build.clj
+++ build.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'slothrop/clj-baseball)
-(def version "0.3.4")
+(def version "0.3.5")
 (def class-dir "target/classes/")
 (def basis (b/create-basis {}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
@@ -16,8 +16,8 @@
                 :lib lib
                 :version version
                 :basis basis
-                :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src"] 
+                :src-dirs ["src" "resources"]})
+  (b/copy-dir {:src-dirs ["src" "resources"] 
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
