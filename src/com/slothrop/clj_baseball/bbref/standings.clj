5f0efe23a95961660e6e6e4fd7cc1e020ab1431f
Ben Lieberman <bhlieberman93@gmail.com>
2023-11-01 16:37:24 -0600

    old standings tables not working for unk reason

--- src/com/slothrop/clj_baseball/bbref/standings.clj
+++ src/com/slothrop/clj_baseball/bbref/standings.clj
@@ -1,12 +1,12 @@
 (ns com.slothrop.clj-baseball.bbref.standings
   (:require [com.slothrop.clj-baseball.bbref.datasource :refer [-get]]
             [clojure.string :as string])
-  (:import [org.jsoup Jsoup]
-           [org.jsoup.nodes Document]))
+  (:import [org.jsoup.nodes Document]))
 
 (def url "http://www.baseball-reference.com/leagues/MLB/%s-standings.shtml")
 
-(defn get-standings-html [year]
+(defn get-standings-html ^Document
+  [year]
   (-get (format url year)))
 
 (defn get-tables [html season]
@@ -19,50 +19,24 @@
                    true identity)]
       (for [table tables
             :let [headings (map #(.text %)
-                                (-> table 
+                                (-> table
                                     (.getElementsByTag "tr")
                                     first
                                     (.getElementsByTag "th")))
                   body (first (.getElementsByTag table "tbody"))]]
-        (for [row (.getElementsByTag body "tr")
-              :let [cols (.getElementsByTag row "td")
-                    col-text (map (comp string/trim #(.text %)) cols)]]
-          (cons (-> row (.getElementsByTag "a") first .text string/trim) col-text))))))
+        (into [headings]
+              (for [row (.getElementsByTag body "tr")
+                    :let [cols (.getElementsByTag row "td")
+                          col-text (map (comp string/trim #(.text %)) cols)]]
+                (cons (-> row
+                          (.getElementsByTag "a")
+                          first
+                          .text
+                          string/trim) col-text)))))
+    :else (throw (ex-info "this is unimplemented for now, sorry!" {}))))
 
-(def season (get-standings-html 1981))
+(def season (get-standings-html 1900))
 
-(get-tables season 1981)
+(get-tables season 1900)
 
-(comment
-  (map #(.id %) (.select season "table"))
-  #_(for [table (if (= 1981 season) eighty-one tables)
-          :let [headings (.selectXpath table "//tr//th")
-                #_#_[team-names stats] (group-by (fn [coll] (> (count (string/split coll #"\s")) 1)) %)
-                body (.selectXpath table "//tbody//tr")
-                rows (for [row body
-                           :let [cols (.select row "td")]]
-                       (map #(.text %) cols))]]
-      [(map #(.text %) (distinct headings)) rows])
-  (let [headings
-        ["Milwaukee Brewers"
-         "Boston Red Sox"
-         "Detroit Tigers"
-         "Baltimore Orioles"
-         "Cleveland Indians"
-         "New York Yankees"
-         "Toronto Blue Jays"
-         "Tm"
-         "W"
-         "L"
-         "W-L%"
-         "GB"
-         "Kansas City Royals"
-         "Oakland Athletics"
-         "Texas Rangers"
-         "Minnesota Twins"
-         "Seattle Mariners"
-         "Chicago White Sox"
-         "California Angels"
-         "Tm"
-         "W"]]
-    (group-by (fn [el] (> (count (string/split el #"\s")) 1)) (into [] (comp (distinct)) headings))))
+
