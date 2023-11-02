3ea689cdb0cb36b3f50d2d998075a3e2a7e1a768
Ben Lieberman <bhlieberman93@gmail.com>
2023-11-02 14:42:53 -0600

    re-exporting bbref stuff

--- src/com/slothrop/clj_baseball/api.clj
+++ src/com/slothrop/clj_baseball/api.clj
@@ -1,7 +1,12 @@
 (ns com.slothrop.clj-baseball.api
   "Public API for clj-baseball."
+  #_{:clj-kondo/ignore [:unused-namespace :unused-referred-var]}
   (:require [com.slothrop.clj-baseball.statcast.batter :refer [send-req!]]
-            [com.slothrop.clj-baseball.player.lookup :refer [search]]))
+            [com.slothrop.clj-baseball.player.lookup :refer [search]]
+            [com.slothrop.clj-baseball.bbref.league-batting :refer [batting-stats-table]]
+            [com.slothrop.clj-baseball.bbref.team-batting :refer [team-batting]]
+            [com.slothrop.clj-baseball.bbref.team-pitching :refer [team-pitching]]
+            [com.slothrop.clj-baseball.bbref.team-fielding :refer [team-fielding]]))
 
 (defn statcast [params]
   (send-req! params))
