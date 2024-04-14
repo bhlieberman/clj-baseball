2cca1e2fb15c5a45b8b089098c45cb12d99862cf
Ben Lieberman <bhlieberman93@gmail.com>
2024-04-14 12:30:51 -0600

    formatting and added `to-tmd-dataset` conversion function

--- src/com/slothrop/clj_baseball/api.clj
+++ src/com/slothrop/clj_baseball/api.clj
@@ -8,39 +8,43 @@
             [com.slothrop.clj-baseball.bbref.team-pitching :as p]
             [com.slothrop.clj-baseball.bbref.team-fielding :as f]))
 
-(defn statcast 
+(defn statcast
   "Send a query to the Statcast endpoint. Useful keys are:
    `:hfTeam`, `:game-date-lt`, `:game-date-gt`."
   [params]
   (send-req! params))
 
-(defn mlb-player-id-lookup 
+(defn mlb-player-id-lookup
   "Lookup the MLBID of a player (active or otherwise) in the Chadwick Bureau register."
   [params]
   (search params))
 
-(defn league-batting 
+(defn league-batting
   "Return the league batting stats from Baseball Reference between `start-date` and `end-date`."
-  [start-date end-date] 
+  [start-date end-date]
   (lb/batting-stats-table start-date end-date))
 
-(defn team-batting 
+(defn team-batting
   "Return the given `team`'s batting stats from Baseball Reference between `start-date` and `end-date`."
   [team start-date end-date]
   (if (some? end-date)
     (b/team-batting team start-date end-date)
     (b/team-batting team start-date)))
 
-(defn team-pitching 
+(defn team-pitching
   "Return the given `team`'s pitching stats from Baseball Reference between `start-date` and `end-date`."
   [team start-date end-date]
   (if (some? end-date)
     (p/team-pitching team start-date end-date)
     (p/team-pitching team start-date)))
 
-(defn team-fielding 
+(defn team-fielding
   "Return the given `team`'s fielding stats from Baseball Reference between `start-date` and `end-date`."
   [team start-date end-date]
   (if (some? end-date)
     (f/team-fielding team start-date end-date)
     (f/team-fielding team start-date)))
+
+(defn to-tmd-dataset 
+  "Turns a Clojure data structure into a TMD dataset. Provided as a compatibility layer."
+  [s])
